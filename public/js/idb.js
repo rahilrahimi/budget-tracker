17c2c2e1cb73409580cbd94312a625d54be9ede2
Rahil Rahimi <rahilrahimi1362@gmail.com>
2022-01-03 18:46:30 -0500

    revised idb.js file

--- public/js/idb.js
+++ public/js/idb.js
@@ -6,7 +6,7 @@ const request = indexedDB.open("budget_tracker", 1);
 //this event will emit if the database version changes
 request.onupgradeneeded = function (event) {
   // save a reference to the database
-  const db = event.target.result;
+  let db = event.target.result;
   // create an object store (table) called `new_budget`, set it to have an auto incrementing primary key of sorts
   db.createObjectStore("new_budget", { autoIncrement: true });
 
