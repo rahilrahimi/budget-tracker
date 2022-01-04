2dada0418e6d0ef136f33c4d661db2f0e6e89d45
Rahil Rahimi <rahilrahimi1362@gmail.com>
2022-01-03 22:07:42 -0500

    initial change

--- public/js/idb.js
+++ public/js/idb.js
@@ -55,7 +55,7 @@ function uploadBudget() {
   getAll.onsuccess = function () {
     // if there was data in indexedDb's store, let's send it to the api server
     if (getAll.result.length > 0) {
-      fetch("/api", {
+      fetch("/api/transaction/bulk", {
         method: "POST",
         body: JSON.stringify(getAll.result),
         headers: {
